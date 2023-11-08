92e0c36a0fab679beb979ce1aafa7ef4cc17da05
esenturtairuulu8 <esenturtairuulu8@gmail.com>
2023-11-08 11:43:28 +0600

    fixed

--- src/main/java/school/mjc/stage0/conditions/task1/ConditionalTruePrinter.java
+++ src/main/java/school/mjc/stage0/conditions/task1/ConditionalTruePrinter.java
@@ -2,5 +2,10 @@ package school.mjc.stage0.conditions.task1;
 
 public class ConditionalTruePrinter {
     public static void main(String[] args) {
+        boolean result = true;
+
+        if (result) {
+            System.out.println(result);
+        }
     }
 }
